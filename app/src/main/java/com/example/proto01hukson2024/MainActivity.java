9b45f27704df4acce551da691ae16dcda23479b3
Linrin0 <unjou0722@icloud.com>
2024-11-30 11:13:59 +0900

    Initial Commit

--- app/src/main/java/com/example/proto01hukson2024/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.proto01hukson2024;
-
-import android.app.Activity;
-
-public class MainActivity extends Activity {
-}
--- app/src/main/java/com/example/proto01hukson2024/MainActivity.kt
+++ app/src/main/java/com/example/proto01hukson2024/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.speechrecognizer
+package com.example.proto01hukson2024;
 
 import android.content.Intent
 import android.os.Bundle
